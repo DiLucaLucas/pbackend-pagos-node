4419d9a8973f28d9d9a116988450852c68b70a29
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-12-07 17:14:57 -0300

    `Feat: config.js y index.js` | Configura las variables de entorno, inicializa la conexión a la base de datos y sincroniza los modelos. Configura el servidor Express y lo arranca en el puerto definido.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const app = require("./app");
+const { connectDB, sequelize } = require("./database/database");
+require("./models/relaciones.js");
+
+const main = async () => {
+  try {
+    await connectDB();
+    // Sincronizamos los modelos
+    await sequelize.sync();
+    console.log("Modelos sincronizados con la BD");
+
+    app.listen(app.get("port"), () => {
+      console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
+    });
+  } catch (error) {
+    console.log("Error al iniciar el servidor", error);
+    process.exit(1);
+  }
+};
+
+main();
