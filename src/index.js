5c1e98a475dcaa0b40b33945fe50070207b6cc9e
Lucas Di Luca <lucasmartindiluca@gmail.com>
2024-12-09 08:50:21 -0300

    `Feat: Datos Iniciales` | Agregada inicialización de datos esenciales (roles, métodos de pago y estados de pago) para garantizar la correcta configuración inicial del sistema.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const app = require("./app");
 const { connectDB, sequelize } = require("./database/database");
 require("./models/relaciones.js");
+const datosIniciales = require("./database/datosIniciales");
 
 const main = async () => {
   try {
     await connectDB();
+
     // Sincronizamos los modelos
     await sequelize.sync();
     console.log("Modelos sincronizados con la BD");
+
+    await datosIniciales();
 
     app.listen(app.get("port"), () => {
       console.log(`Servidor corriendo en el puerto ${app.get("port")}`);
